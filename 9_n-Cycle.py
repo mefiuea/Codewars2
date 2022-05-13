c0a15020270ca8478e2b2ded50693388c0b2aba5
Maciej Koczot <mefiuea@gmail.com>
2022-05-13 12:55:20 +0200

    add task

--- 9_n-Cycle.py
+++ 9_n-Cycle.py
@@ -1,17 +1,23 @@
 import math
+from decimal import Decimal, getcontext
+import gmpy2
+from gmpy2 import mpfr
+from mpmath import *
 
 
 # TODO nie działa dla dużych liczb - trzeba poprawić
 def cycle(n):
+    getcontext().prec = 150
     check_str = ''
     i = 4
-    res = 1 / n
+    print(getcontext())
+    res = Decimal(1 / n)
     print(res)
 
     res_str = str(res)
     print(res_str)
     rest = math.modf(res)[0]
-    print(rest)
+    print('REST:', rest)
 
     while i < len(res_str):
         check_str = res_str[2:i]
@@ -29,19 +35,66 @@ def cycle(n):
 # print(cycle(7))
 # print(cycle(18118))
 # print(cycle(37))
-# print(cycle(197))
+print(cycle(197))
+print('------------------')
+
+
+def cycle_1(n):
+    i = 4
+    mp.dps = 200
+    result = mpf(1) / mpf(n)
+    result_str = str(result)
+
+    while i < len(result_str):
+        check_str = result_str[2:i]
+        check_str_len = len(check_str)
+        if check_str == result_str[i:i + check_str_len]:
+            return check_str_len
+        i += 1
+    return -1
+
+
+print(cycle_1(197))
+print('-------------------')
+
 
 def cycle_2(n):
-    check_str = ''
     i = 4
-    result = 1 / n
-    print(result)
+    gmpy2.get_context().precision = 1024
+    result = (mpfr(1) / mpfr(n))
+    result_str = str(result)
 
-    print(result * 10, ' ', int(result * 10))
-    print(result * 100, ' ', int(result * 100))
-    print(result * 1000, ' ', int(result * 1000))
-    print(result * 10000, ' ', int(result * 10000))
-    print(result * 100000, ' ', int(result * 100000))
+    while i < len(result_str):
+        check_str = result_str[2:i]
+        check_str_len = len(check_str)
+        if check_str == result_str[i:i + check_str_len]:
+            return check_str_len
+        i += 1
+    return -1
 
 
-print(cycle_2(33))
+print(cycle_2(197))
+print('--------------')
+
+
+def cycle_3(n):
+    check_str = ''
+    store_dictionary = {}
+    result = 1 % n
+
+    while ((result != 0) and (result not in store_dictionary)):
+        store_dictionary[result] = len(check_str)
+        result = result * 10
+
+        check_str_part = result // n
+        check_str += str(check_str_part)
+        result = result % n
+
+    if result == 0:
+        return -1
+    else:
+        return len(check_str[store_dictionary[result]:])
+
+
+print(cycle_3(197))
+print('--------------')
